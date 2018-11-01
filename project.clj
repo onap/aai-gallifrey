65635eb379813c675a3d70b6ed9e6acc3268df5e
sblimkie <steven.blimkie@amdocs.com>
2018-11-01 16:40:39 -0400

    upversion to 1.3.1
    Change-Id: I122c7806a6568afcb15b24543b50e659ae8be0ae
    Issue-ID: AAI-1817
    Signed-off-by: sblimkie <steven.blimkie@amdocs.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onap.aai/gallifrey "1.3.0-SNAPSHOT"
+(defproject org.onap.aai/gallifrey "1.3.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
 
                  [com.7theta/utilis "1.0.4"]
